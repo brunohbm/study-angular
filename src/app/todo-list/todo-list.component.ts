ff6033edc29db741e5bbf1905b1d9ea77180e542
Bruno Henrique <brunohbm23@gmail.com>
2024-08-08 20:25:44 -0300

    study: Adding dynamic data

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TodoListItemComponent } from "../todo-list-item/todo-list-item.component";
+
+@Component({
+  selector: 'todo-list',
+  standalone: true,
+  imports: [TodoListItemComponent],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent {
+}
