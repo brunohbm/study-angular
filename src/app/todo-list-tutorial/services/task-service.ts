6d1b91d653706a315e4dd1e2a6c5893b0baad7c1
Bruno Henrique <brunohbm23@gmail.com>
2024-08-12 20:15:00 -0300

    feat: adicao da descricao

--- src/app/todo-list-tutorial/services/task-service.ts
+++ src/app/todo-list-tutorial/services/task-service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+// O decorator @Injectable({ providedIn: 'root' }) torna a classe um serviço singleton disponível em 
+// toda a aplicação Angular. Ele facilita o gerenciamento de dependências e otimiza o código, removendo \
+// serviços não utilizados durante a build.
 @Injectable({
     providedIn: 'root',
 })
